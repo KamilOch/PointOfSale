93e151a5d18d2a1ef5c4b60b8e3f916c834b5012
il <ilmak@o2.pl>
2019-04-09 13:18:40 +0200

    mock objects for tests

--- src/test/java/FirstMockitoTest.java
+++ src/test/java/FirstMockitoTest.java
@@ -9,32 +9,42 @@ import static org.mockito.Mockito.when;
 
 public class FirstMockitoTest {
 
-    Printer printer = new Printer();
+    PrinterInt printer = new Printer();
     LcdDisplayInt display = new Monitor();
     BarCodesScannerInt scanner = new Scanner();
     List<Product> products = new ArrayList<>();
 
-    PointOfSale posMock = new PointOfSale(printer, display, scanner, products);
+    PointOfSale posTest = new PointOfSale(printer, display, scanner, products);
+
+    PrinterInt printerMock = mock(Printer.class);
+    LcdDisplayInt displayMock = mock(Monitor.class);
+    BarCodesScannerInt scannerMock = mock(Scanner.class);
+
+
+
 
     @Test
     public void test(){
         assertTrue(true);
     }
-
+/*
     @Test
     public void letsMockCheckBarCodeTrue(){
 
-        PointOfSale posMock = mock(PointOfSale.class);
-        when(posMock.checkBarCode("1111"));
+        //PointOfSale posMock = mock(PointOfSale.class);
+        when(posTest.checkBarCode("1111"));
         assertTrue(true);
     }
     @Test
     public void letsMockCheckBarCodeFalse(){
 
-        PointOfSale posMock = mock(PointOfSale.class);
-        when(posMock.checkBarCode("")).thenReturn(false);
+        //PointOfSale posMock = mock(PointOfSale.class);
+        when(posTest.checkBarCode("")).thenReturn(false);
         assertFalse("Invalid bar-code",false);
         assertFalse(false);
     }
+*/
+
+
 
 }
